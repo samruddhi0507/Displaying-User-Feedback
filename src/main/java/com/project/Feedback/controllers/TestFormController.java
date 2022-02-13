a54bcd9c56544e7153a9e056e53dc0cb10fe5593
Samruddhi Randive <samruddhirandive97@gmail.com>
2022-02-13 13:37:07 +0530

    Changes have beeen Committed

---
+++ src/main/java/com/project/Feedback/controllers/TestFormController.java
@@ -0,0 +1,36 @@
+package com.project.Feedback.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.project.Feedback.entities.Feedback;
+import com.project.Feedback.services.FeedbackService;
+
+@Controller
+public class TestFormController {
+
+	@Autowired
+	FeedbackService feedbackService;
+	
+	@GetMapping("/test_form")
+	public String showTestForm(ModelMap model) {
+		model.addAttribute("test", new Feedback());
+		return "testformjsp";
+	}
+	
+	
+	@PostMapping("/test_form")
+	public String submitTestForm(@ModelAttribute("testUser") Feedback fb, ModelMap m) {
+		feedbackService.addNewFeedback(fb);
+		m.addAttribute("test", fb);
+			return "post";
+	}
+		
+
+
+}
+
